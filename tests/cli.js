4fb5b250adce16e929a37d548fc1b8e6f9da9891
Jon Wire <iambipedal@gmail.com>
2024-10-03 21:20:15 -0500

    disable v1 tests in CLI

--- tests/cli.js
+++ tests/cli.js
@@ -1,5 +1,8 @@
 import QUnit from 'qunit';
 import './util/cli-shim.js';
-import './index.js';
+
+// no longer testing v1 on the CLI, since jsdom is failing a percentage of
+// tests that pass correctly in the browser.
+import './v2/index.js';
 
 global.QUnit = QUnit;
