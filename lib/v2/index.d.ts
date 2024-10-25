0ff4803842060a30c6d7f8521c53e1ea852ee7f0
Jon Wire <iambipedal@gmail.com>
2024-10-24 21:37:34 -0500

    fix discoverability of types ...

---
+++ lib/v2/index.d.ts
@@ -0,0 +1,8 @@
+import * as types from './types';
+
+declare const html: types.html;
+declare const text: types.textElementBuilder;
+declare const attribute: types.attribute;
+declare const node: types.node;
+declare const id: types.id;
+declare const list: types.list;
