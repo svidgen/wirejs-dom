d5b3aee9b2a912d7c2511e28e67767c67a84e584
Jon Wire <iambipedal@gmail.com>
2024-11-21 21:03:05 -0600

    adds useJSDOM for ssr,ssg,test contexts; bumped patch number

--- tests/util/cli-shim.js
+++ tests/util/cli-shim.js
@@ -1,10 +1,4 @@
 import { JSDOM } from 'jsdom';
+import { useJSDOM } from '../../lib/v2/index.js';
 
-const DOM = new JSDOM('<!doctype html><html><body></body></html>', { pretendToBeVisual: true });
-global.window = DOM.window;
-global.document = window.document;
-global.Element = window.Element;
-global.Node = window.Node;
-global.NodeList = window.NodeList;
-global.DOMParser = DOM.window.DOMParser;
-global.MutationObserver = DOM.window.MutationObserver;
+useJSDOM(JSDOM);
